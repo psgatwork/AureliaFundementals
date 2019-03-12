fe072accf2870b441b1153742be84f0f1ad0d814
psgatwork <psgatwork@gmail.com>
2019-03-12 10:10:29 +0530

    First commit all

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+app.use(express.static('./'));
+var server = app.listen(2112, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("server started ", host, port);
+});
---
+++ src/app.js
@@ -0,0 +1,6 @@
+export class App{
+    constructor(){
+        this.message = "Test 2";
+        this.imageurl = "https://img.icons8.com/flat_round/64/000000/stork-with-bundle.png";
+    }
+}
