59c8b4690ceaf7e0ff3532d1e138ff95eccd08de
psgatwork <psgatwork@gmail.com>
2019-03-16 00:17:24 +0530

    Updated main.js with configure function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-export class Main{
-    constructor(){
-        
-    }
+import {LogManager} from 'aurelia-framework';
+import {ConsoleAppender} from 'aurelia-logging-console';
+
+LogManager.addAppender(new ConsoleAppender());
+LogManager.setLevel(LogManager.logLevel.debug);
+
+export function configure(aurelia) {
+    aurelia.use.standardConfiguration();
+    aurelia.start().then(x => x.setRoot("app",document.body));
 }
