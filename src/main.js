94455db99e11e8eacabbf02a1eeebdb350b694c5
psgatwork <psgatwork@gmail.com>
2019-03-17 16:08:50 +0530

    Comments added

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ LogManager.addAppender(new ConsoleAppender());
 LogManager.setLevel(LogManager.logLevel.debug);
 
 export function configure(aurelia) {
+
+    // Inform to use standered configurations.
+    // if not mentioned, have to config everything.
     aurelia.use.standardConfiguration();
+
+    // SetRoot function. this refers to the immidiate component to bind here
+    // and to what element it should target to.
     aurelia.start().then(x => x.setRoot("app",document.body));
 }
