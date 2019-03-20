605ce89b1d701f26df6b4968d28544d5f0068fe7
psgatwork <psgatwork@gmail.com>
2019-03-20 21:52:53 +0530

    injected property in use

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import { serviceOne } from "serviceOne";
 import { inject } from "aurelia-framework";
 
-@inject(serviceOne)
+// @inject(serviceOne)
 export class App {
-    constructor() {
+    // Inject ServiceOne to viewModel of app
+    static inject = [serviceOne];
+    constructor(serviceOne) {
+        
+        // Use Property of injected service
+        serviceOne.serviceData.push("one");
+
+        // Log the service
+        console.log("from app");
+        console.log(serviceOne.serviceData)
+        
         this.message = "Test 2";
         this.imageurl = "https://img.icons8.com/flat_round/64/000000/stork-with-bundle.png";
     }
--- src/serviceOne.js
+++ src/serviceOne.js
@@ -1,5 +1,10 @@
 export class serviceOne {
     constructor() {
+        // Define property
+        this.serviceData = [];
+
+        // Log the property
         console.log("service constructor");
+        console.log(this.serviceData);
     }
 }
