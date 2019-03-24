b7325bca106cb58093dfda580874a56bc90cf973
psgatwork <psgatwork@gmail.com>
2019-03-24 12:39:29 +0530

    Routing implementation success

--- src/shell.js
+++ src/shell.js
@@ -1,5 +1,12 @@
-export class Shell{
-    constructor(){
+export class Shell {
+    constructor() {
         this.shellValue = "parent variable";
+    }
+    configureRouter(config, router) {
+        this.router = router;
+        config.title= "New Route Module";
+        config.map([
+            {route:'', moduleId:'eventSection'}
+        ]);
     }
 }
